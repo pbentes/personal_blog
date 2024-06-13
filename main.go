1f1caf1e0d41630292f1ee49118486b294e53015
pbentes <pedrogomes2000.3@gmail.com>
2024-06-13 17:06:35 +0100

    Using pocketbase + templ to do SSG

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	// serves static files from the provided public dir (if exists)
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+
+Use pocketbase as a content management system:
+	- Start Pocketbase X
+	- Create a Post collection X
+
+The idea is that I can ´go run´ to start pocketbase then kill it and ´go run generate´ to build the static site into the output folder.
+
+*/
